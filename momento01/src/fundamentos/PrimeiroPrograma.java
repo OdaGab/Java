2796cddbd3193c92ada69b0ed61935a7da9f9f27
Odair Gabriel <32102507+OdaGab@users.noreply.github.com>
2025-03-29 13:28:52 -0300

    Primeiro Programa

---
+++ momento01/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package fundamentos;
+
+/**
+ *  Professor Odair Gabriel 
+ *  contato.
+ *  odavita32@gmail.com
+ *  Meu primeiro programa 
+ *  Atalhos importante 
+ *  syso ctrol + espasso aparecera 
+ *  System.out.println("");
+ */
+public class PrimeiroPrograma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Prof. Odair Gabriel");
+		
+		
+	}
+
+}
